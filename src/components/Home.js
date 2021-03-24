e4628ee3902f0c691ce2a6dcad4881b16fee27b5
DarijAli <darijali@hotmail.com>
2021-03-24 18:21:18 +0500

    Added animation to base,home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <motion.div className="home container" animate={{}}>
+    <motion.div
+      className="home container"
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+    >
       <h2 animate={{ fontSize: 50 }}>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button animate={{}}>Create Your Pizza</motion.button>
